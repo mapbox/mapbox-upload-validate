8c89150a30577ea9f667baa02ae73ea596bd2412
Nina Armah <nina@mapbox.com>
2016-08-17 16:57:33 -0400

    match error message

--- test/validators.mbtiles.test.js
+++ test/validators.mbtiles.test.js
@@ -91,7 +91,7 @@ test('lib.validators.mbtiles: null tile', function(t) {
 test('lib.validators.mbtiles: no tile_data column', function(t) {
   validate(fixtures.invalid.mbtiles.notiledata, function(err) {
     t.ok(err, 'expected error');
-    t.equal(err.code, 'EINVALID', 'expected error code');
+    t.equal(err.code, 'SQLITE_ERROR', 'expected error code');
     t.equal(err.message, expected.mbtilesErrors.nulltile, 'expected error message');
     t.end();
   });
