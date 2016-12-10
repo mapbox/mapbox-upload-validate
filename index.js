a964edaed75c45d19dfbcec3f7d592c4b7295f0a
Dane Springmeyer <dane@mapbox.com>
2016-12-09 16:32:07 -0800

    optimize validation steps

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var validate = require('./lib/validate');
-var Step = require('step');
 
 module.exports = function(filepath, callback) {
   var results = {};
@@ -9,31 +8,8 @@ module.exports = function(filepath, callback) {
     if (err && err.code === 'EINVALID') return callback(null, false, err.message);
     return callback(err);
   }
-
-  Step(
-    function() {
-      validate.filepath(filepath, this);
-    },
-    function(err, protocol) {
-      if (err) return fail(err);
-      results.filepath = filepath;
-      results.protocol = protocol;
-      results.uri = protocol + '//' + filepath;
-      validate.info(results.uri, this);
-    },
-    function(err, info) {
-      if (err) return fail(err);
-      results.info = info;
-      validate.source(results.uri, this);
-    },
-    function(err, source) {
-      if (err) return fail(err);
-      results.source = source;
-      validate[results.protocol.slice(0,-1)](results, this);
-    },
-    function(err) {
-      if (err) return fail(err);
-      callback(null, true);
-    }
-  );
+  return validate(filepath,function(err) {
+    if (err) return fail(err);
+    return callback(null,true);
+  });
 };
