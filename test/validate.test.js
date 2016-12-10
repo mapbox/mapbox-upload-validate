665f4827934d364f053df87e057e1da169485e0a
Dane Springmeyer <dane@mapbox.com>
2016-12-09 17:54:49 -0800

    fix linting

--- test/validate.test.js
+++ test/validate.test.js
@@ -1,25 +1,8 @@
 var test = require('tape').test;
 var validate = require('../lib/validate');
 var fixtures = require('./fixtures');
-//var expected = require('./expected');
-var queue = require('queue-async');
-//var testUtils = require('./util');
 
 process.env.MapboxAPIMaps = 'https://api.tiles.mapbox.com';
-
-var validFiletypes = Object.keys(fixtures.valid);
-var validProtocols = validFiletypes.map(function(k) {
-  if (k.indexOf('mbtiles') === 0) return 'mbtiles:';
-  if (k === 'csv') return 'omnivore:';
-  if (k === 'shp') return 'omnivore:';
-  if (k === 'tif') return 'omnivore:';
-  if (k === 'geojson') return 'omnivore:';
-  if (k === 'kml') return 'omnivore:';
-  if (k === 'gpx') return 'omnivore:';
-  if (k === 'tilejson') return 'tilejson:';
-  if (k === 'tm2z') return 'tm2z:';
-  if (k.indexOf('serialtiles') === 0) return 'serialtiles:';
-});
 
 test('lib.validate.filepath: unsupported file', function(t) {
   validate(fixtures.invalid.unsupported, function(err, protocol) {
